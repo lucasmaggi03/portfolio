69746aed9dbc70a14eea562461d5bb030f96d150
Lucas <lucasmaggi03@gmail.com>
2025-05-18 16:32:27 -0300

    adding database.json to replace an object in frontend

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,42 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Projects.css";
 import { FaGithub } from "react-icons/fa";
-
-const projects = [
-  {
-    title: "UTimbaN",
-    description:
-      "Interactive platform with payments, authentication, and virtual currencies.",
-    tags: ["React", "TypeScript", "Sequelize", "MercadoPago"],
-    github: "https://github.com/starboyagus/DSW-TP-24",
-  },
-  {
-    title: "TakeFee",
-    description:
-      "Management and fee collection system for clubs or communities.",
-    tags: ["React", "TypeScript", "Node.js", "MySQL"],
-    github: "https://github.com/lucasmaggi03/debt-collection-software",
-  },
-  {
-    title: "Picadito",
-    description: "Online booking for sports fields and complexes.",
-    tags: ["React", "TypeScript", "Node.js", "MySQL"],
-    github: "https://github.com/lucasmaggi03/picadito-frontend",
-  },
-  {
-    title: "Wordle Clone",
-    description: "Clone of the popular word guessing game.",
-    tags: ["React", "TypeScript"],
-    github: "https://github.com/lucasmaggi03/wordle-clone",
-  },
-];
+import db from "../../data/db.json"
 
 const Projects: React.FC = () => {
+  const [data, setData] = useState(db.projects);
+
+  useEffect(() => {
+    setData(db.projects);
+  }, []);
+
   return (
     <section id="projects" className="projects-section">
       <h1 className="projects-title">&lt;Projects/&gt;</h1>
       <div className="projects-grid">
-        {projects.map((project, idx) => (
+        {data.map((project, idx) => (
           <div key={idx} className="card">
             <div className="mac-header">
               <span className="red"></span>
